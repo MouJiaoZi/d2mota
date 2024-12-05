67d9a5cff14ed23c2ed194c2558d6d0c6092b5b7
MouJiaoZi <ww64646464@vip.qq.com>
2024-12-05 19:02:53 +0800

    1

--- game/scripts/src/blocks/0base_block.ts
+++ game/scripts/src/blocks/0base_block.ts
@@ -3,6 +3,7 @@ class CBaseMTBlock {
      *创建基础方块
      * @param iBoardIndex 棋盘索引
      * @param iGridIndex 格子索引
+     * @param bTreavalable 是否可通行
      */
-    constructor(iBoardIndex: number, iGridIndex: number) {}
+    constructor(iBoardIndex: number, iGridIndex: number, bTreavalable: boolean) {}
 }
